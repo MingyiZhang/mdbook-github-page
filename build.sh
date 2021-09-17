d20e8988737426738c1fcdd82b7aa80618a4fcaa
MingyiZhang <mingyizhang1987@gmail.com>
2021-09-17 15:05:18 +0200

    Update build script: create new branch for gh pages

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-git checkout gh-pages
-git merge main --no-edit
+git branch -D gh-pages
+git checkout -b gh-pages
 mdbook build documents --dest-dir ../docs
 git add docs
 git commit -m "Update docs web"
-git push -u origin gh-pages
+git push -u origin gh-pages --force
 
 git checkout main
