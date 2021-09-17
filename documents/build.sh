494237a22717851d8260d97a4525a727caf5183a
MingyiZhang <mingyizhang1987@gmail.com>
2021-09-17 20:22:40 +0200

    Move build.sh to documents

--- documents/build.sh
+++ documents/build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-branch_name="gh-pages"
-branch=$(git branch --list $branch_name)
-
-if [[ -z $branch ]]; then
-  git branch -D gh-pages
-fi
-
+git branch -D gh-pages
 git checkout -b gh-pages
 mdbook build documents
 git add docs
