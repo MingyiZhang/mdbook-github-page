108c9b988a66524dcac63ed1be75cbc56c7ed692
MingyiZhang <mingyizhang1987@gmail.com>
2021-09-20 20:23:20 +0200

    Remove CNAME

--- documents/build.sh
+++ documents/build.sh
@@ -3,7 +3,6 @@
 git branch -D gh-pages
 git checkout -b gh-pages
 mdbook build documents
-cp documents/CNAME docs/CNAME
 git add docs
 git commit -m "Update docs web"
 git push -u origin gh-pages --force
