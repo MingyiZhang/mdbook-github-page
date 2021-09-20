8fa87fee527c8940d88590170cd67fd5a90b2911
MingyiZhang <mingyizhang1987@gmail.com>
2021-09-20 17:46:37 +0200

    Move CNAME to documents

--- documents/build.sh
+++ documents/build.sh
@@ -3,6 +3,7 @@
 git branch -D gh-pages
 git checkout -b gh-pages
 mdbook build documents
+cp documents/CNAME docs/CNAME
 git add docs
 git commit -m "Update docs web"
 git push -u origin gh-pages --force
