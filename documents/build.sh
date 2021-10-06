3db7bcc021b6e6f01babbdc4adc1cbd1db4b0672
MingyiZhang <mingyizhang1987@gmail.com>
2021-10-07 00:13:25 +0200

    Update build

--- documents/build.sh
+++ documents/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-git branch -D gh-pages
-git checkout -b gh-pages
+latest_tag=$(git describe --abbrev=0 --tags)
+gh_pages="gh-pages"
+
+git checkout $gh_pages
+git merge main --no-commit --no-ff
 mdbook build documents
 git add docs
-git commit -m "Update docs web"
+git commit -m "Update docs web $latest_tag"
 git push -u origin gh-pages --force
 
 git checkout main
